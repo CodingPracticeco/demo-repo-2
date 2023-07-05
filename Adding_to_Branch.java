e6ffdd72f3287571e2f2133c27c58168bc4d4f19
somecoffeecups <somecoffeecups@gmail.com>
2023-07-04 21:05:16 -0400

    edited some java code and Readme

---
+++ Adding_to_Branch.java
@@ -0,0 +1,18 @@
+//Testing java stuffff
+public class Adding_to_Branch{
+    public static void main(String[] args) {
+        int[] numbers = new int[15];
+    
+    
+        for(int i; i < numbers.length-1; i++){
+            numbers[i] = i;
+            System.out.println(numbers[i]);
+        }
+        System.out.println(Arrays.toString(numbers));
+
+    }
+
+
+
+
+}
